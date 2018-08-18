1a763c7cb97f9673e1249097a9dd28c9e2950015
Hugo R <hugo.raguin@gmail.com>
2018-08-18 17:46:59 +0200

    Mouvement basique

---
+++ JeuxAout/Assets/Scipts/PlayerController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public float runSpeed;
+    public float jumpForce;
+
+    private float hAxes;
+
+    private Rigidbody2D rb2d;
+
+
+	void Start () {
+        rb2d = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        hAxes = Input.GetAxis("Horizontal");
+        rb2d.velocity = new Vector2(hAxes * runSpeed, rb2d.velocity.y);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rb2d.velocity = new Vector2(rb2d.velocity.x, jumpForce);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        
+    }
+}
