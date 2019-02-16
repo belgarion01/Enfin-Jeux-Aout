f25c8b58d7513df4cad4e8634a8c7cc1ec02bae9
PC Portable Hugo <hugo.raguin@gmail.com>
2019-02-16 19:20:31 +0100

    Finalisation menu

---
+++ JeuxAout/Assets/Scipts/ButtonMenu.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonMenu : MonoBehaviour {
+
+    private Animator anim;
+    private bool hover;
+
+	void Start () {
+        anim = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        anim.SetBool("Hover", hover);
+	}
+
+    private void OnMouseEnter()
+    {
+        hover = true;
+    }
+
+    private void OnMouseExit()
+    {
+        hover = false;
+    }
+}
